98847f7f77424c2656a56cab3440b2e31792444c
josediegohdezalejo-web <josediegohdezalejo@gmail.com>
2026-03-16 15:15:40 -0400

    Add files via upload

---
+++ android/app/src/main/kotlin/com/mediasis/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mediasis.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+/**
+ * MainActivity principal de MediAsis.
+ * 
+ * Esta clase es el punto de entrada de la aplicación Android
+ * y extiende FlutterActivity para ejecutar la aplicación Flutter.
+ */
+class MainActivity: FlutterActivity()

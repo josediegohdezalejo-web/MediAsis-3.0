8a9ae313d9ef40567e7d8ec42463c089c62ef254
josediegohdezalejo-web <josediegohdezalejo@gmail.com>
2026-03-16 16:25:35 -0400

    Refactor comments in widgets.dart
    Removed unnecessary comments and cleaned up the file.

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,7 +1,5 @@
-/// Widgets reutilizables de MediAsis
-/// 
-/// Este archivo exporta todos los widgets utilizados en la aplicación
-/// para facilitar su importación en otros módulos.
+// Widgets reutilizables de MediAsis
+// Este archivo exporta todos los widgets utilizados en la aplicación
 
 export 'mediasis_logo.dart';
 export 'common_widgets.dart';
